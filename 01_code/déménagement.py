8d743fa4f7dbf6fa7cf76203b3dcd079c7eabc7e
Jules Musquin <jules.musquin@chartes.psl.eu>
2026-06-02 15:24:10 +0200

    déménagement gros bordel

--- 01_code/déménagement.py
+++ 01_code/déménagement.py
@@ -1,9 +1,40 @@
 import os
 from pathlib import Path
 
-input_dir : Path(.)
+directories_dict = {
+"aikon_dir" : Path("Aikon"),
+"horae_dir" : Path("HoraeV2"),
+"illu_dir" : Path("illuhisdoc"),
+"sved_dir" : Path("S-VED"),
+}
 
-for filename in os.listdir(input_dir):
-    if filename.endswith(".jpg"):
-        filename.cwd()
+index = 0
+
+for key, value in directories_dict.items():
+    if key != "aikon_dir":
+        print(f"{key} : {value}")
+        img_dir = Path(f"{value}/images")
+        labels_dir = Path(f"{value}/labels")
+        if os.path.exists(img_dir):
+            print("ok")
+
+        else:
+            print("fuuuuck")
+
+    else: 
+        manifests = set(i for i in os.listdir(value))
+        print(len(manifests))
+        for manifest in manifests:
+            directory ={ 
+                f"{manifest}" : f"{os.listdir(os.path.join(value, manifest))}"
+                        }
+
+        print(directory)
+                
+        
+                          
+        
+
+    
+
         
